18c3ca45d6b7f30b0dfcdd9695c5f16bfe74d898
spankov <spankov@ikod.kz>
2024-10-08 10:35:47 +0500

    feat: implement programm start and stop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+import printGreeting from "./utils/printGreeting.js";
+import process from 'node:process';
+
+process.stdin.resume();
+printGreeting();
+
+process.stdin.on('data', (input) => {
+    const command = input.toString().trim();
+
+    if (command === '.exit') {
+        printGreeting(true);
+    }
+});
+
+process.on('SIGINT', () => {
+    printGreeting(true);
+})
